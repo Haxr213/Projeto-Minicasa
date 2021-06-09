f65d520eee7cfd7135542fd7119eae3592e74ae7
RafaelBorges666 <73503058+RafaelBorges666@users.noreply.github.com>
2021-06-09 00:40:55 -0300

    Update ImagemHelper.h

--- RenderSweetHome/ImagemHelper.h
+++ RenderSweetHome/ImagemHelper.h
@@ -6,8 +6,5 @@ ref class ImagemHelper{
 
 public:
 	void aplicaEfeitos(string nomeArquivo, string diretorio, int criterio);
-	void escalaCinza(string diretorio, int* buffer, int width, int height);
-	void aumentaContraste(string diretorio, int* buffer, int width, int heigh, int maxColor);
-	void giroHorizontal(string diretorio, int* buffer, int width, int height);
 };
 
